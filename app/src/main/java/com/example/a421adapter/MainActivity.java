b453ae7f7a2690920da63ef4c5246fa97a0cd471
RodionKunafin <george.t.o.p@mail.ru>
2020-04-15 11:24:30 +0300

    работает

--- app/src/main/java/com/example/a421adapter/MainActivity.java
+++ app/src/main/java/com/example/a421adapter/MainActivity.java
@@ -11,17 +11,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.content.ContextCompat;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
-    // Генератор случайностей
+
     private Random random = new Random();
-    // Наш адаптер
+
     private ItemsDataAdapter adapter;
-    // Список картинок, которые мы будем брать для нашего списка
+
     private List<Drawable> images = new ArrayList<>();
 
     @Override
@@ -36,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
         setSupportActionBar(toolbar);
         fillImages();
 
-        // При тапе по кнопке добавим один новый элемент списка
+
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -44,21 +46,19 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        // Создаем и устанавливаем адаптер на наш список
+
         adapter = new ItemsDataAdapter(this, null);
         listView.setAdapter(adapter);
 
-        // При тапе по элементу списка будем показывать его данные
+
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                // Тут мы получаем и отображаем данные,
-                // но можно сделать и перейти в новую активити с этими данными
                 showItemData(position);
             }
         });
 
-        // При долгом тапе по элементу списка будем удалять его
+
         listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
             @Override
             public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
@@ -68,8 +68,7 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    // Заполним различными картинками, которые встроены в сам Android
-    // ContextCompat обеспечит нам поддержку старых версий Android
+
     private void fillImages() {
         images.add(ContextCompat.getDrawable(MainActivity.this,
                 android.R.drawable.ic_menu_report_image));
@@ -83,10 +82,6 @@ public class MainActivity extends AppCompatActivity {
                 android.R.drawable.ic_menu_call));
     }
 
-    // Создадим ну почти случайные данные для нашего списка.
-    // random.nextInt(граница_последнего_элемента)
-    // Для каждого элемента мы возьмем 1 случайную картинку
-    // из 5, которые мы сделали вначале.
     private void generateRandomItemData() {
         adapter.addItem(new ItemData(
                 images.get(random.nextInt(images.size())),
@@ -95,7 +90,6 @@ public class MainActivity extends AppCompatActivity {
                 random.nextBoolean()));
     }
 
-    // Покажем сообщение с данными
     private void showItemData(int position) {
         ItemData itemData = adapter.getItem(position);
         Toast.makeText(MainActivity.this,
